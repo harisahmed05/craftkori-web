d1b085a5b3d00a2953fa25fb7bf9c3fae9ea917e
harisahmed05 <ahsanharisahmed@gmail.com>
2025-07-12 15:39:25 +0600

    Initial commit

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+    "html/template"
+    "log"
+    "net/http"
+)
+
+func main() {
+    // Serve static files
+    http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
+    tmpl := template.Must(template.ParseFiles("index.html"))
+
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        err := tmpl.Execute(w, nil)
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+        }
+    })
+
+    log.Println("Starting server on :8080")
+    if err := http.ListenAndServe(":8080", nil); err != nil {
+        log.Fatal("ListenAndServe: ", err)
+    }
+}
